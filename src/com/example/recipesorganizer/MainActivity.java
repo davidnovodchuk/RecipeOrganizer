33e068ec5961f434ce9b16d5ee7a1f7218d7fff0
davidnovodchuk <david.novodchuk@gmail.com>
2014-12-02 15:51:02 -0500

    Update MainActivity.java

--- src/com/example/recipesorganizer/MainActivity.java
+++ src/com/example/recipesorganizer/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,72 +14,85 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 	ListView listView ;
 	private static final int ACTIVITY_CREATE=0;
-    private static final int ACTIVITY_EDIT=1;
+	private static final int ACTIVITY_EDIT=1;
 
-    private static final int INSERT_ID = Menu.FIRST;
-    private static final int DELETE_ID = Menu.FIRST + 1;
-    private DBAdapter mDbHelper;
+	private static final int INSERT_ID = Menu.FIRST;
+	private static final int DELETE_ID = Menu.FIRST + 1;
+	private DBAdapter mDbHelper;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mDbHelper = new DBAdapter(this);
-        mDbHelper.open();
+		mDbHelper.open();
 
-		 // Get ListView object from xml
-        listView = (ListView) findViewById(R.id.recipeList);
-        
-        Cursor recipesCursor = mDbHelper.fetchAllNotes();
-        
-        String[] title = null;
-        if(recipesCursor.moveToFirst())
-        {
-        	do {
-        		title = new String[]{DBAdapter.KEY_TITLE};
-        	} while (recipesCursor.moveToLast());
-        }
+		// Get ListView object from xml
+		listView = (ListView) findViewById(R.id.recipeList);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, title);
+		Cursor recipesCursor = mDbHelper.fetchAllNotes();
 
-        // Assign adapter to ListView
-        listView.setAdapter(adapter); 
-        
-        // ListView Item Click Listener
-        listView.setOnItemClickListener(new OnItemClickListener() {
+		String[] title = null;
+		if(recipesCursor.moveToFirst())
+		{
+			do {
+				title = new String[]{DBAdapter.KEY_TITLE};
+			} while (recipesCursor.moveToLast());
+		}
 
-              @Override
-              public void onItemClick(AdapterView<?> parent, View view,
-                 int position, long id) {
-                
-               // ListView Clicked item index
-               // int itemPosition     = position;
-               
-               // ListView Clicked item value
-               String  itemValue    = (String) listView.getItemAtPosition(position);
-                  
-               // Moving to the RecipeActivity and passing selected recipe name
-               Intent intent = new Intent("com.example.recipesorganizer.RecipeActivity");
-               intent.putExtra(DBAdapter.KEY_ROWID, id);
-               startActivity( intent );
-               
-              }
+		if( title != null ) {
+			
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+					android.R.layout.simple_list_item_1, android.R.id.text1, title);
 
-         });
+			// Assign adapter to ListView
+			listView.setAdapter(adapter); 
+
+			// ListView Item Click Listener
+			listView.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+
+					// ListView Clicked item index
+					// int itemPosition     = position;
+
+					// ListView Clicked item value
+					String  itemValue    = (String) listView.getItemAtPosition(position);
+
+					// Moving to the RecipeActivity and passing selected recipe name
+					Intent intent = new Intent("com.example.recipesorganizer.RecipeActivity");
+					intent.putExtra(DBAdapter.KEY_ROWID, id);
+					startActivity( intent );
+
+				}
+
+			});
+		}
+		else {
+			
+			RelativeLayout rr = new RelativeLayout(this);
+			TextView tv = new TextView(this);
+			tv.setText("No Recipes in the Database!");
+			rr.addView(tv);
+			
+			setContentView(rr);
+		}
 	}
-	
-	 
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	    // Inflate the menu items for use in the action bar
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main, menu);
-	    return super.onCreateOptionsMenu(menu);
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
@@ -91,11 +105,11 @@ public class MainActivity extends ActionBarActivity {
 		if (id == R.id.action_settings) {
 			return true;
 		}
-		*/
+		 */
 		if (id == R.id.action_new) {
 			Intent intent = new Intent("com.example.recipesorganizer.ChooseAddOptionActivity");
-            intent.putExtra("recipe_name", "test");
-            startActivity( intent );
+			intent.putExtra("recipe_name", "test");
+			startActivity( intent );
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
